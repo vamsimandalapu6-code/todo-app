4e826ae4ba57089210ef3dd8331f7388e2a50661
vamsimandalapu6-code <vamsimandalapu6@gmail.com>
2026-06-22 12:11:43 +0530

    Add files via upload

---
+++ todo.py
@@ -0,0 +1,34 @@
+
+tasks=[]
+def menu():
+    print("1.add tasks")
+    print("2.view tasks")
+    print("3.update tasks")
+    print("4.delete tasks")
+    print("5.exit tasks")
+
+while True:
+    menu()
+    
+    choice=input("enter the chioce(1-5)")
+    if choice=="1":
+        task_name=input("enter task name: ")
+        tasks.append(task_name)
+    elif choice=="2":
+        for task in tasks:
+            print(task)
+    elif choice=="3":
+        for i,task in enumerate(tasks,1):
+            print(i,task)
+        num=int(input("enter the number to update task"))
+        new_task=input("enter the updated task: ")
+        tasks[num-1]=new_task
+        
+    elif choice=="4":
+        for i,task in enumerate(tasks,1):
+            print(i,task)
+        num=int(input("enter the delete task number:"))
+        tasks.pop(num-1)
+    else:
+        print("exit tasks")
+        break
