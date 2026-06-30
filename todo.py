c6ddd1c27ede21efee2633b2396066166ac7fe1c
vamsimandalapu6-code <vamsimandalapu6@gmail.com>
2026-06-30 14:44:28 +0530

    Add files via upload

--- todo.py
+++ todo.py
@@ -1,5 +1,11 @@
+import json
 
-tasks=[]
+
+try:
+    with open("tasks.json", "r") as f:
+        tasks = json.load(f)
+except:
+    tasks = []
 def menu():
     print("1.add tasks")
     print("2.view tasks")
@@ -14,6 +20,8 @@ while True:
     if choice=="1":
         task_name=input("enter task name: ")
         tasks.append(task_name)
+        with open("tasks.json", "w") as f:
+            json.dump(tasks, f)
     elif choice=="2":
         for task in tasks:
             print(task)
@@ -23,12 +31,17 @@ while True:
         num=int(input("enter the number to update task"))
         new_task=input("enter the updated task: ")
         tasks[num-1]=new_task
+        with open("tasks.json", "w") as f:
+            json.dump(tasks, f)
         
     elif choice=="4":
         for i,task in enumerate(tasks,1):
             print(i,task)
         num=int(input("enter the delete task number:"))
         tasks.pop(num-1)
+        with open("tasks.json", "w") as f:
+            json.dump(tasks, f)
+        
     else:
         print("exit tasks")
         break
